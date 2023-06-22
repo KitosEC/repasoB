60436865c671f2bb08b3b1db051d62ad748b3f8e
master <master@SMLAB108PC23_23>
2023-06-22 17:56:55 -0500

    Agregar archivos de proyecto.

---
+++ repasoB/repasoB/Page2.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace repasoB
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page2 : ContentPage
+
+    {
+        string usuario;
+        public Page2(string usuario, string contrasena)
+        {
+            InitializeComponent();
+            lblusuario.Text = "usuario conectado" + usuario;
+            this.usuario = usuario;
+        }
+
+        private void txtDato_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            //rango del 1 al 100
+            try
+            {
+                double dato = Convert.ToDouble(txtDato.Text);
+                if(dato > 100)
+                {
+                    DisplayAlert("Alerta", "fuera de rango", "cerrar");
+                }
+            }
+            catch (Exception)
+            {
+
+            
+            }
+        }
+
+        private void btnAbrir_Clicked(object sender, EventArgs e)
+        {
+            double dato = Convert.ToDouble(txtDato.Text);
+            string estudiante = pkEstudiantes.SelectedItem.ToString();
+
+            Navigation.PushAsync(new Page3(usuario, dato, estudiante));
+        }
+    }
+}
---
+++ repasoB/repasoB/page1.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace repasoB
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class page1 : ContentPage
+    {
+        public page1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIniciar_Clicked(object sender, EventArgs e)
+        {
+            string usuario = "uisrael";
+            string contrasena = "2023";
+            if (txtUsuario.Text == usuario && txtContrasena.Text == contrasena)
+            {
+                Navigation.PushAsync(new Page2(usuario, contrasena));
+            }
+            else
+            {
+                DisplayAlert("Alerta", "usuario o clave incorrecta", "cerrar");
+            }
+        }
+
+        private void ImageButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
